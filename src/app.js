f94d97e944f9748f941862857b3d6e72b1adf77f
LeoGourven <leo.gourven@gmail.com>
2015-08-06 09:27:42 +0200

    add content to modale artist (wip)

--- src/app.js
+++ src/app.js
@@ -23,7 +23,31 @@ const populateModal = function(id){
   const artist = findArtist(artists, id)
   const modal = dom("#artist-modal")
 
-  modal.select('.artist-name').textContent = artist.name
+  // reset
+  modal.select('.party').removeClass('live')
+  modal.select('.party').removeClass('night')
+  modal.select('.party').removeClass('playground')
+  modal.select('.youtube iframe').attr("src", "#")
+  modal.select('.soundcloud iframe').attr("src", "#")
+
+  // populate
+  modal.select('.artist-name').html(artist.name)
+  modal.select('.pic img').attr('src', `/assets/modale/${artist.pic}`)
+  modal.select('.party').html(`Echap ${artist.party}`)
+  modal.select('.party').addClass(artist.party)
+  modal.select('.place p').html(artist.place)
+  modal.select('.place span').html(artist.date)
+  modal.select('.youtube iframe').attr("src", `https://www.youtube.com/embed/${artist.id_youtube}`)
+  modal.select('.soundcloud iframe').attr("src", `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${artist.id_soundcloud}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`)
+  if(artist.label){
+    modal.select('.description .head span:first-child').html(`${artist.location} – ${artist.label}`)  
+  }else{
+    modal.select('.description .head span:first-child').html(`${artist.location}`)
+  }
+  
+  modal.select('.description .head span:last-child').html(artist.music_type)
+  modal.select('.description p').html(artist.description)
+  
   toggleModal(modal)
   
 }
@@ -57,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       html += `
         <li class="${artist.party}" data-id="${artist.id}">
-          <p class="pic" style="background-image: url(/assets/artists/${artist.pic})"></p>
+          <p class="pic" style="background-image: url(/assets/artists/mono-${artist.pic})"></p>
           <p>${artist.name}</p>
         </li>
       `
